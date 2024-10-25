890555c04cf7e1a9bbbd362d1eb3614d61480e6f
ACJT123 <acjtacjt@gmail.com>
2024-10-25 22:32:53 +0800

    fix: dont use auth request for non-user microservices

--- auth-service/routes/auth.js
+++ auth-service/routes/auth.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {
-  verify,
-  generateToken,
-} = require("../services/jwt");
+const { verify, generateToken } = require("../services/jwt");
 
 router.get("/sign", (req, res) => {
   const { userid } = req.headers;
@@ -49,6 +46,8 @@ router.get("/refresh", (req, res) => {
 router.get("/verify", (req, res) => {
   let token = req.header("Authorization");
 
+  console.log("token", token);
+
   if (!token) {
     return res.status(400).send("Token is required");
   }
@@ -57,7 +56,7 @@ router.get("/verify", (req, res) => {
     token = token.split(" ")[1];
     const decoded = verify(token);
 
-    res.status(200).json({ decoded });
+    res.status(200).json({ userId: decoded.userId });
   } catch (error) {
     console.error(error);
 
