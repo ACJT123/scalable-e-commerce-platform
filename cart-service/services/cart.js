6227f0d2f76175855fa9d98ec668c030eede4a39
ACJT123 <acjtacjt@gmail.com>
2024-10-26 15:24:29 +0800

    feat(cart-service): should return product details when get the cart

--- cart-service/services/cart.js
+++ cart-service/services/cart.js
@@ -1,10 +1,35 @@
 const Cart = require("../models/cart");
-const CustomError = require("../libs/Error");
+const axios = require("axios");
 
 const getCart = async (userId) => {
-  const cart = await Cart.findOne({ userId });
+  let cart = await Cart.findOne({ userId });
+
+  const details = await Promise.all(
+    cart.products.map(async (product) => {
+      const details = await _getDetails(product.productId);
+      
+      return details;
+    })
+  );
+
+  // convert the cart object to non mongoose object
+  cart = cart.toObject();
+
+  cart.products = cart.products.map((product, index) => ({
+    ...product,
+    name: details[index].name,
+    price: details[index].price,
+  }));
 
   return cart;
+};
+
+const _getDetails = async (productId) => {
+  const response = await axios.get(
+    `http://localhost:80/api/product-catalog/product/${productId}/`
+  );
+
+  return response.data;
 };
 
 const addToCart = async (userId, productId, quantity) => {
